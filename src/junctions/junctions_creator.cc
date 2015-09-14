0c85209fc140bf23f7af1d4340c9ca8430fc89d9
Avinash Ramu <aramu@genome.wustl.edu>
2015-09-14 11:19:48 -0500

    minor edit

--- src/junctions/junctions_creator.cc
+++ src/junctions/junctions_creator.cc
@@ -71,7 +71,7 @@ int JunctionsCreator::parse_options(int argc, char *argv[]) {
     cerr << endl << "Minimum junction anchor length: " << min_anchor_length;
     cerr << endl << "Minimum intron length: " << min_intron_length;
     cerr << endl << "Maximum intron length: " << max_intron_length;
-    cerr << endl << "BAM file: " << bam_;
+    cerr << endl << "Alignment: " << bam_;
     cerr << endl << "Output file: " << output_file;
     cerr << endl;
     return 0;
