7f4359a91aeb09f84b8d115e1ca8713910afa77f
Avinash Ramu <aramu@genome.wustl.edu>
2015-09-15 16:38:08 -0500

    add tests

--- tests/integration-test/test_junctions_create.py
+++ tests/integration-test/test_junctions_create.py
@@ -16,8 +16,33 @@ class TestCreate(IntegrationTest, unittest.TestCase):
             params = ["junctions", "create", anchor, "-o", output_file, bam1]
             rv, err = self.execute(params)
             self.assertEqual(rv, 0)
-            #self.assertEqual('', err)
             self.assertFilesEqual(expected_file, output_file)
+
+    def test_junctions_create_intron_size(self):
+        bam1 = self.inputFiles("test_hcc1395.bam")[0]
+        output_file = self.tempFile("create.out")
+        min_intron = "8039"
+        max_intron = "8039"
+        expected_file = self.inputFiles("junctions-create/expected-i" +
+                min_intron + "-I" + max_intron +
+                ".out")[0]
+        params = ["junctions", "create", "-o", output_file,
+                  "-i", min_intron, "-I", max_intron, bam1]
+        rv, err = self.execute(params)
+        self.assertEqual(rv, 0)
+        self.assertFilesEqual(expected_file, output_file)
+
+    def test_region(self):
+        bam1 = self.inputFiles("test_hcc1395.bam")[0]
+        output_file = self.tempFile("create.out")
+        region = "1:22405013-22405020"
+        expected_file = self.inputFiles("junctions-create/expected-r" +
+                region + ".out")[0]
+        params = ["junctions", "create", "-o", output_file, "-r", region,
+                  bam1]
+        rv, err = self.execute(params)
+        self.assertEqual(rv, 0)
+        self.assertFilesEqual(expected_file, output_file)
 
     def test_no_bam(self):
         output_file = self.tempFile("create.out")
