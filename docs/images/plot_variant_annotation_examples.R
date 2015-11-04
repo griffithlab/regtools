d36d3decda045ad52a883283a77cafaa84647f79
Avinash Ramu <trackavinash@gmail.com>
2015-11-04 11:44:39 -0600

    Update docs and add variants-annotate page

---
+++ docs/images/plot_variant_annotation_examples.R
@@ -0,0 +1,33 @@
+#! /usr/bin/Rscript
+
+library(ggplot2)
+library(grid)
+
+#https://stackoverflow.com/questions/25106508/ggplot2-is-there-an-easy-way-to-wrap-annotation-text
+wrapper <- function(x, ...) paste(strwrap(x, ...), collapse = "\n")
+
+#Setup plot skeleton
+p <- ggplot() + ylim(30, 55) + theme(axis.text = element_blank()) + xlab("") + ylab("")+ theme(axis.ticks = element_blank()) + scale_x_continuous(breaks=-10:70) + ggtitle("Variant annotation")
+
+#Topmost transcript
+p <- p + geom_rect(aes(xmin = 10, ymin = 40, xmax = 30, ymax = 50)) + geom_rect(aes(xmin = 40, ymin = 40, xmax = 60, ymax = 50)) + geom_segment(aes(x = 30, y = 45, xend = 40, yend = 45), arrow = arrow(length = unit(0.5, "cm")))
+
+#Plot variants
+p <- p + geom_rect(aes(xmin = 22, ymin = 34, xmax = 23, ymax = 35), fill = "red")
+p <- p + geom_rect(aes(xmin = 27, ymin = 33, xmax = 28, ymax = 34), fill = "red")
+p <- p + geom_rect(aes(xmin = 31, ymin = 32, xmax = 32, ymax = 33), fill = "red")
+p <- p + geom_rect(aes(xmin = 35, ymin = 31, xmax = 36, ymax = 32), fill = "red")
+
+#Add labels
+p <- p + annotate("text", label = "Transcript", x = 20, xmin = 10, y = 51.5, size = 5)
+p <- p + annotate("text", label = "Variants", x = 20, xmin = 10, y = 36.5, size = 5)
+p <- p + annotate("text", label = "Distance=6;", x = 44, xmin = 44, y = 34.6, size = 4)
+p <- p + annotate("text", label = "Distance=2;splicing_exonic", x = 48.5, xmin = 40, y = 33.6, size = 4)
+p <- p + annotate("text", label = "Distance=2;splicing_intronic", x = 48.8, xmin = 40, y = 32.6, size = 4)
+p <- p + annotate("text", label = "Distance=5;", x = 44, xmin = 44, y = 31.6, size = 4)
+
+#Add lines to make it clear
+p <- p + geom_vline(xintercept = 30, linetype = "longdash") + geom_vline(xintercept = 40, linetype = "longdash")
+
+print(p)
+ggsave("variant_annotation_examples.png")
