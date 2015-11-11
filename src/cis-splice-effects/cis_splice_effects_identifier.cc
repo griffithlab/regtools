a2c9741b9c1b888fa675eb5e43f2799c3b85f9ae
Avinash Ramu <aramu@genome.wustl.edu>
2015-11-11 12:42:49 -0600

    Reorganize cis-splice-effects files

---
+++ src/cis-splice-effects/cis_splice_effects_identifier.cc
@@ -0,0 +1,36 @@
+/*  cis_splice_effects_identifier.cc -- 'cis-splice-effects identify' methods
+
+    Copyright (c) 2015, The Griffith Lab
+
+    Author: Avinash Ramu <aramu@genome.wustl.edu>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.  */
+
+#include "cis_splice_effects_identifier.h"
+#include "junctions_annotator.h"
+#include "junctions_extractor.h"
+#include "variants_annotator.h"
+
+void CisSpliceEffectsIdentifier::parse_options(int argc, char* argv[]) {
+    std::cerr << "inside parse options\n";
+}
+
+void CisSpliceEffectsIdentifier::read_variants() {
+    std::cerr << "Inside read variants\n";
+}
