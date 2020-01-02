2742db2b8be6f3cc9d25c3f379161ee757650624
Kelsy Cotto <kelsy.cotto@wustl.edu>
2020-01-02 12:44:08 -0600

    tiny fix

--- scripts/stats_wrapper.py
+++ scripts/stats_wrapper.py
@@ -20,31 +20,31 @@ args = input_parser.parse_args()
 tag = args.tag
 cohort_dir = args.cohort_directory
 
-for tag in tags:
-    lines_per_file = 50000
-    smallfile = None
-    with open(f'{cohort_dir}/all_splicing_variants_{tag}.tsv', 'r') as bigfile:
-        for lineno, line in enumerate(bigfile):
-            if lineno % lines_per_file == 0:
-                if smallfile:
-                    smallfile.close()
-                small_filename = 'small_file_{}.txt'.format(lineno + lines_per_file)
-                smallfile = open(small_filename, "w")
-            smallfile.write(line)
-        if smallfile:
-            smallfile.close()
-    #get chunks
-    files = glob.glob('small_file_*')
-    for file in files:
-        subprocess.run(f'Rscript --vanilla /home/ec2-user/workspace/regtools/scripts/compare_junctions_hist_v2.R {tag} {file}')
-    output_files = glob.glob("*_out.tsv")
-    output_files.sort()  # glob lacks reliable ordering, so impose your own if output order matters
-    with open(f'junction_pvalues_{tag}.tsv', 'wb') as outfile:
-        for i, fname in enumerate(output_files):
-            with open(fname, 'rb') as infile:
-                if i != 0:
-                    infile.readline()  # Throw away header on all but first file
-                # Block copy rest of file from input to output without parsing
-                shutil.copyfileobj(infile, outfile)
-                print(fname + " has been imported.")
-    os.remove('small_file*')
+lines_per_file = 50000
+smallfile = None
+with open(f'{cohort_dir}/all_splicing_variants_{tag}.tsv', 'r') as bigfile:
+    for lineno, line in enumerate(bigfile):
+        if lineno % lines_per_file == 0:
+            if smallfile:
+                smallfile.close()
+            small_filename = 'small_file_{}.txt'.format(lineno + lines_per_file)
+            smallfile = open(small_filename, "w")
+        smallfile.write(line)
+    if smallfile:
+        smallfile.close()
+#get chunks
+files = glob.glob('small_file_*')
+for file in files:
+    subprocess.run(f'Rscript --vanilla /home/ec2-user/workspace/regtools/scripts/compare_junctions_hist_v2.R {tag} {file}')
+output_files = glob.glob("*_out.tsv")
+output_files.sort()  # glob lacks reliable ordering, so impose your own if output order matters
+with open(f'junction_pvalues_{tag}.tsv', 'wb') as outfile:
+    for i, fname in enumerate(output_files):
+        with open(fname, 'rb') as infile:
+            if i != 0:
+                infile.readline()  # Throw away header on all but first file
+            # Block copy rest of file from input to output without parsing
+            shutil.copyfileobj(infile, outfile)
+            print(fname + " has been imported.")
+os.remove('small_file*')
+
