53cea48133ea2febf22e6f276295be1c6a802f3c
Kelsy Cotto <kelsy.cotto@wustl.edu>
2020-01-03 13:46:22 -0600

    Update stats_wrapper.py

--- scripts/stats_wrapper.py
+++ scripts/stats_wrapper.py
@@ -36,7 +36,7 @@ with open(f'all_splicing_variants_{tag}.bed', 'r') as bigfile:
 files = glob.glob('small_file_*')
 files.sort()
 for file in files:
-    subprocess.run(f'Rscript --vanilla /home/ec2-user/regtools/scripts/compare_junctions_hist_v2.R {tag} {file}', shell=True, check=True)
+    subprocess.run(f'Rscript --vanilla /home/ec2-user/workspace/regtools/scripts/compare_junctions_hist_v2.R {tag} {file}', shell=True, check=True)
 output_files = glob.glob("*_out.tsv")
 output_files.sort()  # glob lacks reliable ordering, so impose your own if output order matters
 with open(f'junction_pvalues_{tag}.tsv', 'wb') as outfile:
