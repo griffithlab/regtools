e391d5acb669efb09509547cb43060d240c53ac0
Kelsy Cotto <kelsy.cotto@wustl.edu>
2020-01-05 13:35:56 -0600

    minor fix

--- scripts/stats_wrapper.py
+++ scripts/stats_wrapper.py
@@ -17,7 +17,7 @@ args = input_parser.parse_args()
 tag = args.tag
 cwd = os.getcwd()
 
-lines_per_file = 50000
+lines_per_file = 25000
 smallfile = None
 num_small_file = 0
 with open(f'all_splicing_variants_{tag}.bed', 'r') as bigfile:
@@ -35,18 +35,23 @@ with open(f'all_splicing_variants_{tag}.bed', 'r') as bigfile:
 #get chunks
 files = glob.glob('small_file_*')
 files.sort()
+number_of_in_files = len(files)
 for file in files:
     subprocess.run(f'Rscript --vanilla /home/ec2-user/workspace/regtools/scripts/compare_junctions_hist_v2.R {tag} {file}', shell=True, check=True)
 output_files = glob.glob("*_out.tsv")
-output_files.sort()  # glob lacks reliable ordering, so impose your own if output order matters
-with open(f'compare_junctions/hist/junction_pvalues_{tag}.tsv', 'wb') as outfile:
-    for i, fname in enumerate(output_files):
-        with open(fname, 'rb') as infile:
-            if i != 0:
-                infile.readline()  # Throw away header on all but first file
-            # Block copy rest of file from input to output without parsing
-            shutil.copyfileobj(infile, outfile)
-            print(fname + " has been imported.")
+output_files.sort()# glob lacks reliable ordering, so impose your own if output order matters
+number_of_out_files = len(output_files)
+if number_of_in_files == number_of_out_files:
+    with open(f'compare_junctions/hist/junction_pvalues_{tag}.tsv', 'wb') as outfile:
+        for i, fname in enumerate(output_files):
+            with open(fname, 'rb') as infile:
+                if i != 0:
+                    infile.readline()  # Throw away header on all but first file
+                # Block copy rest of file from input to output without parsing
+                shutil.copyfileobj(infile, outfile)
+                print(fname + " has been imported.")
+else:
+    print("Number of output files doesn't match the number of input files that should have been processed")
 files = glob.glob('small_file_*')
 for file in files:
     os.remove(file)
